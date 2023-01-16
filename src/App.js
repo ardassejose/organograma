bd37ad2e1e9e6356c6cafb44bce7cf63b0d179b8
arizona <arizona@ARZIM2564.local>
2023-01-16 17:47:31 -0300

    Removed *debugger* sintax

--- src/App.js
+++ src/App.js
@@ -42,7 +42,6 @@ function App() {
   const [colaboradores, setColaboradores] = useState([])
 
   const aoNovoColaboradorAdicionado = (colaborador) => {
-    debuggre
     setColaboradores([...colaboradores, colaborador])
   }
 
