918c5354ecdc343511a7b856c2fa4e1f3ce58d42
arizona <arizona@ARZIM2564.local>
2023-01-16 17:43:03 -0300

    v1 - Organo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { Fragment, useState } from 'react';
+import Banner from './componentes/Banner';
+import Formulario from './componentes/Formulario';
+import Rodape from './componentes/Rodape';
+import Time from './componentes/Time';
 
 function App() {
+
+  const times = [
+    {
+      nome: 'Hub - Digital',
+      corPrimaria: '#57c278',
+      corSecundaria: '#D9F7E9'
+    },
+    {
+      nome: 'Vivo - CLM',
+      corPrimaria: '#82CFFA',
+      corSecundaria: '#E8F8FF'
+    },
+    {
+      nome: 'XP',
+      corPrimaria: '#D86EBF',
+      corSecundaria: '#FAE9F5'
+    },
+    {
+      nome: 'Bradesco - Aldeiah',
+      corPrimaria: '#E06B69',
+      corSecundaria: '#FDE7E8'
+    },
+    {
+      nome: 'Natura',
+      corPrimaria: '#FEBA05',
+      corSecundaria: '#FFF5D9'
+    },
+    {
+      nome: 'Toyota',
+      corPrimaria: '#A6D157',
+      corSecundaria: '#F0F8E2'
+    },
+  ];
+
+  const [colaboradores, setColaboradores] = useState([])
+
+  const aoNovoColaboradorAdicionado = (colaborador) => {
+    debuggre
+    setColaboradores([...colaboradores, colaborador])
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Banner/>
+      <Formulario times={times.map(time => time.nome)} aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
+      {times.map(time => <Time 
+        key={time.nome} 
+        nome={time.nome} 
+        corPrimaria={time.corPrimaria} 
+        corSecundaria = {time.corSecundaria}
+        colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)}
+      />)}
+      <Rodape/>  
+    </Fragment>
   );
 }
 
---
+++ src/componentes/ListaSuspensa/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './ListaSuspensa.css';
+
+const ListaSuspensa = (props) => {
+  return (
+    <div className='lista-suspensa'>
+      <label>{props.label}</label>
+      <select onChange={evento => props.aoAlterado(evento.target.value)} required={props.required} value={props.valor}>
+        <option value=""></option>
+        {props.itens.map((item, index) => <option key={index}>{item}</option>)}
+      </select>
+    </div>
+  );
+}
+ 
+export default ListaSuspensa;
